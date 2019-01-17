f94a8c4eaa0dc10d1b409060072c78a24b0eb5a5
Carlos <nzlosh@yahoo.com>
2019-01-17 22:32:31 +0100

    Add setup.py for better pip support.

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="err-backend-discord",
+    version="0.0.1",
+    author="err-backend-discord contributors",
+    author_email="nobody@localhost",
+    description="A Discord backend for Errbot.",
+    long_description="Not available",
+    long_description_content_type="text/markdown",
+    url="https://github.com/nzlosh/err-backend-discord",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
