9930e4b5f6adf9375e387c673b9f804b7912986c
Carlos <nzlosh@yahoo.com>
2019-01-18 22:47:03 +0100

    Constrain requirements and set update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 import setuptools
 
 setuptools.setup(
-    name="err-backend-discord",
-    version="0.0.1",
-    author="err-backend-discord contributors",
     author_email="nobody@localhost",
-    description="A Discord backend for Errbot.",
-    long_description="Not available",
-    long_description_content_type="text/markdown",
-    url="https://github.com/nzlosh/err-backend-discord",
-    packages=setuptools.find_packages(),
+    author="err-backend-discord contributors",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
+    description="A Discord backend for Errbot.",
+    keywords='errbot rocket.chat',
+    long_description_content_type="text/markdown",
+    long_description="Not available",
+    name="err-backend-discord",
+    packages=setuptools.find_packages(),
+    url="https://github.com/nzlosh/err-backend-discord",
+    version="0.0.1",
 )
